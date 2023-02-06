9184d9a9f74daf39b5ff25e76bb3a179bf4f3a36
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2023-02-06 23:07:06 +0000

    remove overriding error loging declaration

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-$log_file = "src/logs/php_errors.log";
-ini_set("log_errors", TRUE);
-ini_set('error_log', $log_file);
+// $log_file = "src/logs/php_errors.log";
+// ini_set("log_errors", TRUE);
+// ini_set('error_log', $log_file);
 
 include_once(__DIR__ .'./dotenv.php');
 include_once(__DIR__ .'./DB.php');
