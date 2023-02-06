36cc4ed946bb08209d6db273431137ae9de09402
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2023-02-06 22:55:20 +0000

    fixed a typo in ini_set

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 $log_file = "src/logs/php_errors.log";
-ini_set(“log_errors”, TRUE);
+ini_set("log_errors", TRUE);
 ini_set('error_log', $log_file);
 
 include_once(__DIR__ .'./dotenv.php');
