ae3b44dea71a1e151b4a87701087baaebe582098
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2023-03-02 19:42:39 +0000

    remove commented out explicit error settings for cleanup

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
-// $log_file = "src/logs/php_errors.log";
-// ini_set("log_errors", TRUE);
-// ini_set('error_log', $log_file);
-
-// error_reporting(E_ALL);
 
 include_once($_SERVER['DOCUMENT_ROOT'] .'/src/dotenv.php');
 include_once($_SERVER['DOCUMENT_ROOT'] .'/src/DB.php');
